dd01c62300e202ee6f160925d0afecd7d8129b7a
HarryPotter12352 <harrypottr12352.github@outlook.com>
2024-02-29 18:37:24 +0530

    Update

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
-from sympy import symbols, diff
+from sympy import symbols, diff, exp, I, factorial
 import math
-x = symbols('x')
-y = symbols('y')
-f = math.e**(2*x)
-n = 20
+x, y = symbols('x y')
+f = exp(x)
+n = 100
 a = math.pi
-point_to_evaluate = 2
+point_to_evaluate = math.pi*I
 funcs = [f]
-output = []
-for i in range(1,n+1):
-    f_prime = f # f' = f
-    for j in range(1,i+1):
-        f_prime = diff(f_prime,x)
-    funcs.append(f_prime)
-for i in range(n):
-    output.append(((funcs[i].subs(x,a))/math.factorial(i))*((y-a)**i))
-final = 0
-for i in output:
-    final += i
-print(final.subs(y,point_to_evaluate))
+for i in range(1, n+1):
+    f = diff(f, x)
+    funcs.append(f)
+factorials = [factorial(i) for i in range(n)]
+output = [((funcs[i].subs(x, a))/factorials[i])*((y-a)**i) for i in range(n)]
+final = sum(output)
+print(final.subs(y, point_to_evaluate).evalf())
